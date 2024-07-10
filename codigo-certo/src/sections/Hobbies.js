44f9d859902c36d54db7c29d7d508e8bec22232e
Walace Ramos <contatowalace.dev@gmail.com>
2024-07-10 19:01:39 -0300

    correções ortográficas
    corrigindo algumas palavras

--- codigo-certo/src/sections/Hobbies.js
+++ codigo-certo/src/sections/Hobbies.js
@@ -7,7 +7,7 @@ export const Hobbies = () => {
 
             <p>
                 Além de escrever códigos, também tenho uma grande paixão por música.<br/> Este com certeza foi meu primeiro contato com um mundo no qual poucas pessoas se interessevam.<br/>
-                Com 12 anos começei meus estudos musicais e escolhi o teclado como instrumento, e foi amor a primeira vista, do meu início até hoje, estudei por conta própria instrumentos de cordas, tais como violão e contra-baixo, estes instrumentos aprendi apenas com a curiosidade e algumas páginas rasgadas de um livro de música do meu tio. 
+                Com 12 anos inciei meus estudos musicais e escolhi o teclado como instrumento, e foi amor a primeira vista, do meu início até hoje, estudei por conta própria instrumentos de cordas, tais como violão e contra-baixo, estes instrumentos aprendi apenas com a curiosidade e algumas páginas rasgadas de um livro de música do meu tio. 
                 <img src={music}/>
             </p>
 
--- codigo-certo/src/sections/Objetivos.js
+++ codigo-certo/src/sections/Objetivos.js
@@ -7,7 +7,7 @@ export const Objetivos = () => {
                 <h2>Objetivos</h2>
             </div>
             <p>
-                Tenho estudado incasávelmente com um único propósito: ingressar na área de desenvolvimento.<br/>
+                Tenho estudado incasavelmente com um único propósito: ingressar na área de desenvolvimento.<br/>
                 Minhas últimas semanas tem sido de poucas horas de descanso e muitas horas de estudos, reconheço que só conquistamos aquilo que perseguimos loucamente. <br/>
                 Já se passou o primeiro ano desde que decidi, iniciar meus estudos migrar para área de desenvolvimento, tem sido dificil, mas de uma coisa eu tenho certeza, hoje me sinto mais preparado para me candidatar para vagas de estágio ou emprego dentro da área, por isso, através deste portfólio , busco destacar minhas habilidades e experiências, mostrando todo o meu comprometimento e paixão pela área de tecnologia.
             </p>
