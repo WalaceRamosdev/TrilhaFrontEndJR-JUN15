c9e342ce95e8e02f6aae0605640c00015aa7e53f
Walace Ramos <contatowalace.dev@gmail.com>
2024-07-18 11:46:56 -0300

    update

--- codigo-certo/src/sections/Hobbies.js
+++ codigo-certo/src/sections/Hobbies.js
@@ -3,15 +3,15 @@ import music from '../images/Profile/music.svg'
 
 export const Hobbies = () => {
     return (
-        <div className={style.hobbies} id='hobbies'>
+        <div className={style.hobbies}>
 
-            <p>
+            <p  id='hobbies'>
                 Além de escrever códigos, também tenho uma grande paixão por música.<br/> Este com certeza foi meu primeiro contato com um mundo no qual poucas pessoas se interessevam.<br/>
-                Com 12 anos inciei meus estudos musicais e escolhi o teclado como instrumento, e foi amor a primeira vista, do meu início até hoje, estudei por conta própria instrumentos de cordas, tais como violão e contra-baixo, estes instrumentos aprendi apenas com a curiosidade e algumas páginas rasgadas de um livro de música do meu tio. 
+                Com 12 anos iniciei meus estudos musicais e escolhi o teclado como instrumento, e foi amor a primeira vista, com o tempo estudei por conta própria instrumentos de cordas, tais como violão e contra-baixo, estes instrumentos aprendi apenas com a curiosidade e algumas páginas rasgadas de um livro de música do meu tio. 
                 <img src={music}/>
             </p>
 
-            <h1>
+            <h1 id='hobbies-mobile'>
                 Hobbies &#127929;
             </h1>
             
