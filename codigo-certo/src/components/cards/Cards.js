4918c4e7ab37011ad8cccaad55512aaf3e4eb862
Walace Ramos <contatowalace.dev@gmail.com>
2025-05-01 10:43:27 -0300

    Update Cards.js
    .

--- codigo-certo/src/components/cards/Cards.js
+++ codigo-certo/src/components/cards/Cards.js
@@ -3,13 +3,12 @@ import { Button } from '../buttons/Button'
 
 export const Cards = ({title, img, tech, link}) => {
     return (
+        
         <div className={style.cards}>
-
             <h2>{title}</h2>
                 <img src={img}/>
                 <h3>Tecnologias utilizadas: {tech}</h3>
                 <Button text='Saiba mais' link={link} className={style.btn}/>
-            
         </div>
     )
 }
